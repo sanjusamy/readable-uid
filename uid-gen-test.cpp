23deb0baa8d0c3144373107ea0d06d74807e2e64
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-05 16:59:11 +0530

    add test case to check the enough separators

--- uid-gen-test.cpp
+++ uid-gen-test.cpp
@@ -8,6 +8,13 @@ TEST(UID, UID_is_generated_within_length_limit) {
   ASSERT_TRUE(uid.length() > 0);
 }
 
+TEST(UID, UID_is_generated_has_6_separators) {
+  std::string uid = uidgen();
+  ASSERT_TRUE(strncmp(uid,".",1) != 6);
+}
+
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
