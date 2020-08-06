9ea7bb2470e130d123c7e1b7efb9393d1a1323c2
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-06 16:22:10 +0530

    typo corrected

--- uidgen.h
+++ uidgen.h
@@ -4,7 +4,7 @@
 std::string uidgen() {
   std::string uid = "1.4.444.90004.223.";
   unsigned int suffix = std::rand() * std::rand();
-  std::string sufStr = std::to_string(suffix)
+  std::string sufStr = std::to_string(suffix);
   std::string sep = ".";
   return uid + sufStr.substr(0,5) + sep + sufStr.substr(5,5) ;
 }
